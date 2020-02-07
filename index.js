d2dc3173308f19d29c3b816a0717963e1a60123f
Phoenix181 <aadhishrm11@gmail.com>
2020-02-08 00:30:40 +0530

    Refactoring the etch js implementation

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
 init(16);
 
 function init(gridSize) {
-    const resizeButton = document.getElementById('resize-button');
+    const resizeButton = document.querySelector('#resize-button');
     resizeButton.addEventListener('click', resize);
 
-    const resetButton = document.getElementById('reset-button');
+    const resetButton = document.querySelector('#reset-button');
     resetButton.addEventListener('click', clear);
 
     createGrid(gridSize);
 }
 
 function createGrid(gridSize) {
-    const etchContainer = document.getElementById('container');
+    const etchContainer = document.querySelector('#container');
 
     gridSize = gridSize > 100 ? 100
             : gridSize < 0 ? 1
             : gridSize
 
-    etchContainer.style.cssText = `grid-template: repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr);`;
+    etchContainer.style.gridTemplate =  `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
 
-    for (let i = 1, k = 1; i <= gridSize; i++) {
-        for (let j = 1; j <= gridSize; j++, k++) {
+    for (let i = 1; i <= gridSize*gridSize; i++) {
             const etchItem = document.createElement('div');
-            etchItem.className = "etch-box";
+            etchItem.className = "etch-box unhovered";
             etchItem.addEventListener('mouseenter', etch);
 
             etchContainer.append(etchItem);
-        }
     }
 }
 
 function etch(e) {
-    e.target.style.cssText = 'background-color: black;'
+    e.target.className = "etch-box hovered";
 }
-
-const resizeButton = document.getElementById('resize-button');
-resizeButton.addEventListener('click', resize);
 
 function resize() {
     const newGridSize = prompt("Enter number of grids (Between 1 to 100)", 0);
+
+    removeGrid();
 
     createGrid(newGridSize);
     
     clear();
 }
 
+function removeGrid() {
+    const etchBoxes = document.querySelectorAll('.etch-box');
+
+    etchBoxes.forEach(etchBox => {
+        etchBox.remove();
+    });
+}
+
 function clear() {
     const etchBoxes = document.querySelectorAll('.etch-box');
 
     etchBoxes.forEach(etchBox => {
-        etchBox.style.cssText = 'background-color: white;';
+        etchBox.className = "etch-box unhovered";
     });
 }
